30bbdb1a2b080f04790eb5eb48f7f78cc8349808
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-25 14:24:42 +0000

    Deploy AliceO2Group/QualityControl to github.com/AliceO2Group/QualityControl.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,50 @@
+var NAVTREE =
+[
+  [ "QualityControl", "index.html", [
+    [ "Advanced topics", "md_doc_Advanced.html", null ],
+    [ "Repository backends benchmark", "md_doc_benchmark-repo.html", null ],
+    [ "Developers' tips", "md_doc_DevelopersTips.html", null ],
+    [ "Frequently asked questions", "md_doc_FAQ.html", null ],
+    [ "Modules development", "md_doc_ModulesDevelopment.html", null ],
+    [ "Post-processing", "md_doc_PostProcessing.html", null ],
+    [ "QuickStart", "md_doc_QuickStart.html", null ],
+    [ "New features", "md_doc_ReleaseNotesTemplate.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"PedestalsCheck_8h_source.html",
+"classReceiver.html#a89f2425e76bd1374d2d9548b81cae8d4",
+"classo2_1_1quality__control_1_1postprocessing_1_1Reductor.html#ae85876d95609bbb28157cf4dd3e63cbf",
+"classo2_1_1quality__control__modules_1_1mft_1_1BasicClusterQcCheck.html",
+"classo2_1_1quality__control__modules_1_1tof_1_1TOFCheckRawsMulti.html#a6ad5b75d8828bab5a655361ddfb3d0d6",
+"runDataDump_8cxx.html#a9c6d7f8d628391c1bad26be1f1abc271",
+"testNonEmpty_8cxx.html#a5a04246722d9df39c8e85ad0a84472d8"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
